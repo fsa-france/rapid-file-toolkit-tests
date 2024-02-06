d2587741e31ddd915afab5f84d26906c5edb5264
Laurent <lboschet@free.fr>
2024-02-06 12:30:06 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# rapid-file-toolkit-tests
-Environnement to create a random data set and test Pure Storage Rapid File Toolkit on Flashblade
+# PURE STORAGE RAPID FILE TOOLKIT DEMO
+
+## INTRODUCTION
+
+Data Engineers and Data Scientists day to day tasks require running complex data pipelines to test data ingestion and 
+preparation processes from data coming from very diverse sources (Databases, WebScaraping, Feature stores…) before training 
+a model.
+
+In this context, accelerating these iterative processes and tasks may be crucial to improve the time to market of a model
+that will help to solve a business need of their company.
+
+With our distributed architecture coupled with our optimized meta-data engine, we provide a powerful and simple toolkit 
+that will help to massively improve the data pipelines by reducing the time to use some Linux commands on very large datasets 
+such as find, ls, chown, chmod… in several ways:
+
+- **Time Efficiency**: Accelerating these commands can dramatically reduce the time it takes to perform routine tasks. 
+      For example, speeding up find can greatly decrease the time required to locate specific files or directories within a large directory tree. 
+      Similarly, faster ls commands can quickly list the contents of directories, aiding in navigation and file management.
+
+- **Productivity**: With faster command execution, data engineers can complete tasks more efficiently, leading to increased productivity. 
+      This is particularly beneficial for tasks that involve repetitive operations, such as searching for files or changing permissions across multiple files or directories that can also be automated and run many times.
+
+- **Resource Optimization** : Accelerating commands can help optimize resource utilization on Unix systems. 
+      For instance, improving the performance of find can reduce the system load caused by recursive directory traversals, thereby freeing up system resources for other tasks.
+
+- **Improved Workflow** : Faster command execution contributes to a smoother workflow, allowing data engineers to focus on more critical aspects of their work. 
+      This can enhance overall job satisfaction and job performance.
+
+- **Real-Time Data Processing** : In scenarios where real-time data processing is crucial, such as in streaming data pipelines or time-sensitive analytics, accelerated commands enable data engineers to quickly access and manipulate data without experiencing delays.
+
+- **Responsive System Interaction** : Accelerated commands enhance the responsiveness of the Unix system's command-line interface, providing a seamless user experience. 
+       This is especially valuable for data engineers who frequently interact with the command line as part of their daily workflow.
+
+Overall, accelerating commands like find, ls, chown, chmod, tar, cp… offers tangible benefits for data engineers working with Unix systems, ranging from time savings and increased productivity to improved resource utilization and system responsiveness. These optimizations contribute to a more efficient and effective workflow, ultimately enhancing the data engineer's ability to manage and manipulate data efficiently.
+
+## DATA PREPARATION WITH VDBENCH
+
+1. Install vdbench on your Linux system
+
+2. Use the provided vdbench configuration file 'rapidfile-datascience-folder.vdb' to create a test file tree with 500k 4KB files :
+
+```
+     /datascience-folder
+            ├── dir1                        ===> 1000 folders
+            │   ├── vdb.1_1000.dir          ===> 100 files
+            │   │   ├── vdb_f99900.file
+            │   │   ├── vdb_f99901.file
+            │   │   ├── vdb_f99902.file
+            │   │   ├── vdb_f99903.file
+            .........
+            │   │   ├── vdb_f99998.file
+            │   │   └── vdb_f99999.file
+            │   ├── vdb.1_100.dir           ===> 100 files
+            │   │   ├── vdb_f9900.file
+            │   │   ├── vdb_f9901.file
+            .........
+            │   │   ├── vdb_f9998.file
+            │   │   └── vdb_f9999.file
+            │   ├── vdb.1_101.dir
+            │   │   ├── vdb_f10000.file
+            │   │   ├── vdb_f10001.file
+            .........
+```
+
+You will have 5 root folders with each 1000 sub folders.
+Each sub folder will contain 100 4KB files.
+
+## RAPID FILE TOOLKIT INSTALLATION
+
+1. Access your support.purestorage.com account and search for RApidFile Toolkit in order to download bianries
+
+2. Install the rpm in your Linux system :
+
+    % sudo rpm -Uv rapidfile-toolkit-2.1.1-1.x86_64.rpm
+
+## RAPID FILE TOOLKIT AUTOMATED TESTS
+
+If you want to automate all tests comaring standr Linux commands versus RapidFile Toolkit commands, simply run the 
+rft_automated_tests.sh script and look at the outpout that should look like :
+
+    % ./rft_automated_tests.sh
+
+    % more rft_automated_tests_20240205_213852.txt
+
+## DEMO
+
+A recorded demo is also available to show the benefits of the RapidFile Toolkit coupled with Flashblade.
+
+## RESULTS ON OUR PARIS LAB
+
+### LS/PLS, FIND/PFIND and DU/PDU
+![LS/PLS, FIND/PFIND and DU/PDU](RapidFileToolkitResults1.png)
+
+### HMOD/PCHMOD, CHOWN/PCHMOD
+![CHMOD/PCHMOD, CHOWN/PCHMOD](RapidFileToolkitResults2.png)
+
+### CP/PCOPY, TRA/PTAR and RM/PRM
+
+![CP/PCOPY, TRA/PTAR and RM/PRM](RapidFileToolkitResults3.png)
+
+
+
+
---
+++ rft_automated_tests.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+# Enable debugging
+#set -x
+
+# Path varaibles
+ROOT_PATH="/datascience-folder"
+ROOT_DIR_NAMES="dir"
+
+
+# Define log file path relative to script location
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
+LOG_FILE="$SCRIPT_DIR/rft_automated_tests_$(date +'%Y%m%d_%H%M%S').txt"
+
+# Checking accessibility of the root path
+if [ ! -d "$ROOT_PATH" ]; then
+    echo "Error: Root path '$ROOT_PATH' is not accessible."
+    exit 1
+fi
+
+# Function for ls/pls commands
+run_ls() {
+    echo -e "\n*** RUN LS / PLS ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'ls' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time ls -lR "$ROOT_PATH"/"$ROOT_DIR_NAMES"* | wc -l; } 2>&1 | tee -a "$LOG_FILE"
+    #time ls -lR "$ROOT_PATH"/"$ROOT_DIR_NAMES"* | wc -l | tee -a "$LOG_FILE"
+
+    echo    "*** Executing 'pls' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time pls -lR "$ROOT_PATH"/"$ROOT_DIR_NAMES"* | wc -l; } 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Function for find/pfind commands
+run_find() {
+    echo -e "\n*** RUN FIND / PFIND ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'find' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time find "$ROOT_PATH"/"$ROOT_DIR_NAMES"* -type f -name *gutemb* ; } 2>&1 | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'find' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time pfind "$ROOT_PATH"/"$ROOT_DIR_NAMES"* -type f -name *gutemb* ; } 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Function for du/pdu commands
+run_du() {
+    echo -e "\n*** RUN DU / PDU ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'du' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time du -s "$ROOT_PATH"/"$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'pdu' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time pdu -s "$ROOT_PATH"/"$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Function for chmod/pchmod commands
+run_chmod() {
+    echo -e "\n*** RUN CHMOD / PCHMOD ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'chmod' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time chmod -R 777 "$ROOT_PATH"/"$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'pchmod' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time pchmod -R 644 "$ROOT_PATH"/"$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Function for chown/pchown commands
+run_chown() {
+    echo -e "\n*** RUN CHOWN / PCHOWN ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'chown' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time chown -R pureuser "$ROOT_PATH"/"$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'pchown' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { time pchown -R root "$ROOT_PATH"/"$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Function for cp/pcopy commands
+run_cp() {
+    echo -e "\n*** RUN CP / PCOPY ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'cp' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    for i in {1..5}; do
+        { time cp -r "$ROOT_PATH"/"$ROOT_DIR_NAMES"$i "$ROOT_PATH"/cp_"$ROOT_DIR_NAMES"$i ; } 2>&1 | tee -a "$LOG_FILE"
+    done
+
+    echo "  *** Executing 'pcopy' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    for i in {1..5}; do
+        { time pcopy -r "$ROOT_PATH"/"$ROOT_DIR_NAMES"$i "$ROOT_PATH"/pcopy_"$ROOT_DIR_NAMES"$i ; } 2>&1 | tee -a "$LOG_FILE"
+    done
+}
+
+# Function for tar/ptar commands
+run_tar() {
+    echo -e "\n*** RUN TAR / PTAR ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'tar' Unix Standard command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { cd "$ROOT_PATH"; time tar cf "$ROOT_PATH"/tar.out "$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'ptar' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Running... "
+    { cd "$ROOT_PATH"; time ptar cf "$ROOT_PATH"/ptar.out "$ROOT_DIR_NAMES"* ; } 2>&1 | tee -a "$LOG_FILE"
+}
+
+# Function for rm/prm commands 
+# WE WILL ERASE THE FOLDERS CREATED DURING THE PCOPY  ***
+run_rm() {
+     echo -e "\n*** RUN RM / PRM ***" | tee -a "$LOG_FILE"
+
+    echo "  *** Executing 'rm' Unix Standard command on entire directory structure: "$ROOT_PATH"/cp_"$ROOT_DIR_NAMES"*" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Checking cp_"$ROOT_DIR_NAMES"* to remove... "$ROOT_PATH"/cp_"$ROOT_DIR_NAMES"*"; mount -a
+    for i in {1..5}; do
+        if [ ! -d "$ROOT_PATH"/cp_"$ROOT_DIR_NAMES"$i ]; then
+            echo "Error: "$ROOT_PATH"/cp_"$ROOT_DIR_NAMES"$i does not exists. Exiting."
+            exit 1
+        fi
+        echo "  Running... "
+        { time rm -r "$ROOT_PATH"/cp_"$ROOT_DIR_NAMES"$i ; } 2>&1 | tee -a "$LOG_FILE"
+    done
+
+    echo "  *** Executing 'prm' RapidFile Toolkit Optimized command on entire directory structure: "$ROOT_PATH"/cp_dir*" | tee -a "$LOG_FILE"
+    echo "  Unmounting... "$ROOT_PATH""; umount "$ROOT_PATH"
+    echo "  Remounting... "$ROOT_PATH""; mount -a
+    echo "  Checking pcopy_"$ROOT_DIR_NAMES"* to remove... "$ROOT_PATH"/pcopy_"$ROOT_DIR_NAMES"*"; mount -a
+    for i in {1..5}; do
+        if [ ! -d "$ROOT_PATH"/pcopy_"$ROOT_DIR_NAMES"$i ]; then
+            echo "Error: "$ROOT_PATH"/pcopy_"$ROOT_DIR_NAMES"$i does not exists. Exiting."
+            exit 1
+        fi
+        echo "  Running... "
+        { time prm -r "$ROOT_PATH"/pcopy_"$ROOT_DIR_NAMES"$i ; } 2>&1 | tee -a "$LOG_FILE"
+    done
+    
+}
+
+# Calling functions 
+run_ls
+run_find
+run_du
+run_chmod
+run_chown
+run_cp
+run_tar
+run_rm
+
